9b81304309584af3ef9f26e1831e7f3211210b81
Web99-web <145454081+Web99-web@users.noreply.github.com>
2026-01-23 20:42:37 +0100

    Create ma-tracking.js

---
+++ static/js/ma-tracking.js
@@ -0,0 +1,29 @@
+// MetaAdvisor – manual GA4 page tracking
+(function () {
+  if (typeof gtag !== 'function') return;
+
+  function sendPageView() {
+    gtag('event', 'page_view', {
+      page_location: window.location.href,
+      page_path: window.location.pathname,
+      page_title: document.title
+    });
+  }
+
+  // initial
+  sendPageView();
+
+  let lastPath = location.pathname;
+
+  const observer = new MutationObserver(() => {
+    if (location.pathname !== lastPath) {
+      lastPath = location.pathname;
+      sendPageView();
+    }
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+})();
